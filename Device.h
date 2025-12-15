fb656eebefd10cdf018111e2355fe97dffabcc2a
Quasar0707 <sokal0909sokal@gmail.com>
2025-12-15 14:06:43 +0300

    Merge remote main into local

--- Device.h
+++ Device.h
@@ -14,19 +14,19 @@ private:
   std::exponential_distribution<double> distribution;
   double meanServiceTime;
   double serviceStartTime;
-  double totalTimeBusy; // <-- Новое поле
+  double totalTimeBusy;
 
 public:
   Device(int id, double meanTime);
   void startService(const Request& req, double startTime);
-  void completeService(double endTime); // <-- Изменённая сигнатура
+  void completeService(double endTime);
   bool isAvailable() const;
   int getDeviceId() const;
   bool getIsBusy() const;
   const Request& getCurrentRequest() const;
   double getServiceStartTime() const;
-  double getTotalTimeBusy() const; // <-- Новый геттер
+  double getTotalTimeBusy() const;
   double getServiceTime();
 };
 
-#endif // DEVICE_H
+#endif
